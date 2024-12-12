0b224e8ca788638ebfcae136c22e123f4a3101bb
prathammhavale <prathammhavale38@gmail.com>
2024-12-12 21:22:36 +0530

    changing padding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -108,9 +108,9 @@ function App() {
     <div
       className={`${
         theme === "light" ? "bg-amber-300" : "bg-gray-700"
-      } min-h-screen flex justify-center p-4 roboto-medium  transition-colors ease-in-out duration-300`}
+      } min-h-screen flex justify-center p-3 roboto-medium  transition-colors ease-in-out duration-300`}
     >
-      <div className="flex flex-col gap-2 mt-6 md:mt-20 p-2">
+      <div className="flex flex-col gap-2 mt-6 md:mt-20 ">
         <div className="w-full flex flex-row justify-center gap-2">
           <form
             className=" flex flex-row gap-2 border-[1px] bg-white rounded-md "
